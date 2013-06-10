7d3188794ac5d7f01f1e42b409622083361e2455
Kunleash <thanoskan@yahoo.gr>
2013-06-10 19:00:18 +0300

    Ready Enemy Class with comments

--- Bothlo/src/Enemy.java
+++ Bothlo/src/Enemy.java
@@ -30,16 +30,18 @@ public class Enemy extends Character{
 		stat += level*modifier;
 		return stat;
 	}
-	
+	//Method to make it possible for enemies to Level Up
 	public void levelUp(){
 		level++;
 		life = modify(life, level, lifeModifier);
 		damage = modify(damage, level, damageModifier);
 	}
+	//This is used to inflict damage to Heroes
 	public int getDamage(){
 		return this.damage;
 		
 	}
+	//This is used to decrease Enemy's life according to damage taken
 	public void changeLife(int change){
 		life -= change;
 	}
